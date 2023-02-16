6c75f0a2fa2fb7c9fa7c329a156a787cff021e7c
moon2510 <nguyetminhthibui@gmail.com>
2023-02-16 17:33:20 +0700

    Create SideBar

---
+++ src/components/sidebar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/sidebar.css";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar_wrap">
+      <div className="sidebar">
+        {/* <Link to="/lecturer/" className={mycourse ? "active" : "inactive"}></Link> */}
+        <Link to="" className="active" style={{ textDecoration: 'none' }}>
+          <span>Home</span>
+        </Link>
+        <Link to="" style={{ textDecoration: 'none' }}>
+          <span>Services</span>
+        </Link>
+        <Link to="" style={{ textDecoration: 'none' }}>
+          <span>News</span>
+        </Link>
+        <Link to="" style={{ textDecoration: 'none' }}>
+          <span>Blog</span>
+        </Link>
+        <Link to="" style={{ textDecoration: 'none' }}>
+          <span>Contact</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SideBar from "../components/sidebar";
+
+// import Footer from "./../components/Footer";
+
+const HomePage = () => {
+  window.scrollTo(0, 0);
+  return (
+    <div>
+      <SideBar />
+
+      
+    </div>
+  );
+};
+
+export default HomePage;
